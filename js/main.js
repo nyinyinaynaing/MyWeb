dc8ae04e0f46302e27c72a83affcc80ca7730d2f
NAYNAING <naynaing.kollect@gmail.com>
2024-05-18 22:49:45 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
+/*preloader starts
+const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+//preloader ends*/
+
+
+
+
+});
---
+++ readme.txt
@@ -0,0 +1,6 @@
+
+Font Family
+Barlow (can get from google font)
+
+Color Code
+Color code : #F5F6FB / #2B3D91 / #272833
